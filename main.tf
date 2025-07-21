7b2f99d5063c39d44c32a39742777e1f8502dbad
Christopher Houze <cehouze@gmail.com>
2025-07-21 12:54:24 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.2.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "example2" {
+  bucket = "my-tf-example-bucket"
+}
+
+resource "aws_s3_bucket" "example3" {
+  bucket = "my-tf-example-bucketjk"
+}
+
+provider "aws" {
+  # Configuration options
+}
